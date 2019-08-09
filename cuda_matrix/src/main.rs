47bc1c804d5ffd2cfce8b72d414f5b04e60bdfbf
bumzack <bumzacktrara@gmail.com>
2019-08-10 00:16:35 +0200

    matrix multiplication in the works

--- cuda_matrix/src/main.rs
+++ cuda_matrix/src/main.rs
@@ -17,15 +17,15 @@ pub unsafe extern "ptx-kernel" fn printThreadIndex(
 ) {
     use ptx_support::prelude::*;
 
-//    cuda_printf!(
-//        "printThreadIndex   Hello from block(%lu,%lu,%lu) and thread(%lu,%lu,%lu)\n",
-//        Context::block().index().x,
-//        Context::block().index().y,
-//        Context::block().index().z,
-//        Context::thread().index().x,
-//        Context::thread().index().y,
-//        Context::thread().index().z,
-//    );
+    //    cuda_printf!(
+    //        "printThreadIndex   Hello from block(%lu,%lu,%lu) and thread(%lu,%lu,%lu)\n",
+    //        Context::block().index().x,
+    //        Context::block().index().y,
+    //        Context::block().index().z,
+    //        Context::thread().index().x,
+    //        Context::thread().index().y,
+    //        Context::thread().index().z,
+    //    );
     let ix = Context::thread().index().x + Context::block().index().x * block_dim_x as u64;
     let iy = Context::thread().index().y + Context::block().index().y * block_dim_y as u64;
     let idx = iy * nx as u64 + ix;
@@ -48,18 +48,78 @@ pub unsafe extern "ptx-kernel" fn sumMatrixOnGpu2D(
 ) {
     use ptx_support::prelude::*;
 
-    let ix = (Context::thread().index().x + Context::block().index().x * block_dim_x as u64) as isize;
-    let iy =( Context::thread().index().y + Context::block().index().y * block_dim_y as u64)as isize  ;
-    let idx = iy * nx  as isize+ ix;
-    if ix < nx as isize && iy < ny  as isize{
+    let ix =
+        (Context::thread().index().x + Context::block().index().x * block_dim_x as u64) as isize;
+    let iy =
+        (Context::thread().index().y + Context::block().index().y * block_dim_y as u64) as isize;
+    let idx = iy * nx as isize + ix;
+    if ix < nx as isize && iy < ny as isize {
         *mat_c.offset(idx) = *mat_a.offset(idx) + *mat_b.offset(idx);
     }
 
-//    cuda_printf!("thread_id (%ul, %ul)   block_id (%ul, %ul)   coordinate (%ul, %ul)    global_idx %ul,    a =  %f, b = %f, c = %f\n",
-//            Context::thread().index().x as u32, Context::thread().index().y as u32,
-//            Context::block().index().x as u32 ,  Context::block().index().y as u32,
-//            ix as u32, iy as u32, idx as u32, *mat_a.offset(idx as isize) as f64,  *mat_b.offset(idx as isize) as f64,  *mat_c.offset(idx as isize) as f64);
+    //    cuda_printf!("thread_id (%ul, %ul)   block_id (%ul, %ul)   coordinate (%ul, %ul)    global_idx %ul,    a =  %f, b = %f, c = %f\n",
+    //            Context::thread().index().x as u32, Context::thread().index().y as u32,
+    //            Context::block().index().x as u32 ,  Context::block().index().y as u32,
+    //            ix as u32, iy as u32, idx as u32, *mat_a.offset(idx as isize) as f64,  *mat_b.offset(idx as isize) as f64,  *mat_c.offset(idx as isize) as f64);
 }
 
+#[no_mangle]
+#[cfg(target_os = "cuda")]
+pub unsafe extern "ptx-kernel" fn matrix_mul(
+    mat_a: *const f32,
+    mat_b: *const f32,
+    mat_c: *mut f32,
+    mat_a_row: usize,
+    mat_a_col: usize,
+    mat_b_row: usize,
+    mat_b_col: usize,
+    block_dim_x: u32,
+    block_dim_y: u32,
+    // TODO: add succes boolean as reutn value
+    // success: *mut bool,
+) {
+    use ptx_support::prelude::*;
+
+    if (mat_a_col != mat_b_row) {
+        //     *success = false;
+        return;
+    }
+    //    *success = true;
+
+    let col =
+        (Context::thread().index().x + Context::block().index().x * block_dim_x as u64) as isize;
+    let row =
+        (Context::thread().index().y + Context::block().index().y * block_dim_y as u64) as isize;
+    let idx = row * mat_a_col as isize + col;
+
+//    cuda_printf!(
+//        "YYYYYYYY  col = %ul,  row = %ul,  idx = %ul \n",
+//        col as u32,
+//        row as u32,
+//        idx as u32
+//    );
+
+    if col < mat_b_col as isize && row < mat_a_row as isize {
+        let mut tmp = 0f32;
+        for i in 0..mat_a_col {
+            let idx_a = row * mat_a_col as isize + i as isize;
+            let idx_b = row + i as isize * mat_b_col as isize;
+            cuda_printf!(
+                "XXXXXX col = %ul,  row = %ul,  idx_a = %ul,   idx_b = %ul  \n",
+                col as u32,
+                row as u32,
+                idx_a as u32,
+                idx_b as u32
+            );
+            tmp = tmp + *mat_a.offset(idx_a) * *mat_b.offset(idx_b);
+        }
+        *mat_c.offset(idx) = tmp;
+    }
+
+    //    cuda_printf!("thread_id (%ul, %ul)   block_id (%ul, %ul)   coordinate (%ul, %ul)    global_idx %ul,    a =  %f, b = %f, c = %f\n",
+    //            Context::thread().index().x as u32, Context::thread().index().y as u32,
+    //            Context::block().index().x as u32 ,  Context::block().index().y as u32,
+    //            ix as u32, iy as u32, idx as u32, *mat_a.offset(idx as isize) as f64,  *mat_b.offset(idx as isize) as f64,  *mat_c.offset(idx as isize) as f64);
+}
 
 fn main() {}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[macro_use]
 extern crate rustacuda;
 
-use rustacuda::device::DeviceAttribute;
-use rustacuda::memory::DeviceBox;
-use rustacuda::prelude::*;
 use std::error::Error;
 use std::ffi::CString;
-use std::time::{Duration, Instant};
+use std::time::Instant;
+
+use rustacuda::device::DeviceAttribute;
+use rustacuda::prelude::*;
 
 fn show_device_props(device: &Device) {
     println!("devicename: {}", device.name().unwrap());
@@ -157,8 +157,8 @@ fn add_matrix() -> Result<(), Box<dyn Error>> {
     // Get the first device
     let device = Device::get_device(0)?;
 
-    let nx = 8000;
-    let ny = 6000;
+    let nx = 512;
+    let ny = 512;
     let nxy = nx * ny;
 
     let mut matrix_a = vec![0f32; nxy];
@@ -191,7 +191,7 @@ fn add_matrix() -> Result<(), Box<dyn Error>> {
     // Create a stream to submit work to
     let stream = Stream::new(StreamFlags::NON_BLOCKING, None)?;
 
-    let b = (4, 2, 1);
+    let b = (32, 32, 1);
     let block = (b.0 as u32, b.1 as u32, b.2 as u32);
 
     let g = (
@@ -229,7 +229,7 @@ fn add_matrix() -> Result<(), Box<dyn Error>> {
     let mut out_host = vec![0.0f32; nxy];
     d_matrix_c.copy_to(&mut out_host)?;
 
-//    println!("out_host  = {:?}", out_host);
+    //    println!("out_host  = {:?}", out_host);
 
     let start_cpu = Instant::now();
     let res_cpu = add_matrix_cpu(&matrix_a, &matrix_b, nx, ny);
@@ -248,6 +248,167 @@ fn add_matrix() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn mul_matrix() -> Result<(), Box<dyn Error>> {
+    // Initialize the CUDA API
+    rustacuda::init(CudaFlags::empty())?;
+
+    // Get the first device
+    let device = Device::get_device(0)?;
+
+    // MAtrix A
+    let mat_a_row = 8;
+    let mat_a_col = 6;
+    let mut matrix_a = vec![0f32; mat_a_row * mat_a_row];
+    let mut blupp = 1f32;
+    for elem in matrix_a.iter_mut() {
+        *elem = blupp;
+        blupp = blupp + 1.0;
+    }
+
+    // MAtrix B
+    let mat_b_row = 6;
+    let mat_b_col = 8;
+    let mut matrix_b = vec![0f32; mat_b_col * mat_b_row];
+
+    let mut blupp = 101f32;
+    for elem in matrix_b.iter_mut() {
+        *elem = blupp;
+        blupp = blupp + 1.0;
+    }
+    let mut matrix_c = vec![0f32; mat_a_row * mat_b_col];
+
+    // Create a context associated to this device
+    let _context =
+        Context::create_and_push(ContextFlags::MAP_HOST | ContextFlags::SCHED_AUTO, device)?;
+
+    let mut d_matrix_a = DeviceBuffer::from_slice(&matrix_a)?;
+    let mut d_matrix_b = DeviceBuffer::from_slice(&matrix_b)?;
+    let mut d_matrix_c = DeviceBuffer::from_slice(&matrix_c)?;
+
+    // Load the module containing the function we want to call
+    let module_data = CString::new(include_str!("/tmp/ptx-builder-0.5/cuda_matrix/9132ac0994b05664/nvptx64-nvidia-cuda/release/cuda_matrix.ptx"))?;
+    let module = Module::load_from_string(&module_data)?;
+
+    // Create a stream to submit work to
+    let stream = Stream::new(StreamFlags::NON_BLOCKING, None)?;
+
+    let b = (4, 2, 1);
+    let block = (b.0 as u32, b.1 as u32, b.2 as u32);
+
+    let g = (
+        (mat_a_row as i32 + block.0 as i32 - 1) / block.0 as i32,
+        (mat_b_col as i32 + block.1 as i32 - 1) / block.1 as i32,
+        1 as i32,
+    );
+    let grid = (g.0 as u32, g.1 as u32, 1 as u32);
+
+    println!("block = {:?}, grid = {:?}", block, grid);
+
+    let start = Instant::now();
+    let mut success_gpu = true;
+
+    unsafe {
+        // Launch the `add` function with one block containing four threads on the stream.
+        let res = launch!(module.matrix_mul<<<grid, block, 0, stream>>>(
+            d_matrix_a.as_device_ptr(),
+            d_matrix_b.as_device_ptr(),
+            d_matrix_c.as_device_ptr(),
+            mat_a_row,
+            mat_a_col,
+            mat_b_row,
+            mat_b_col,
+            block.0,
+            block.1
+        ));
+
+        match res {
+            Ok(_o) => println!("everything ok"),
+            Err(e) => println!("an error occured: {}", e),
+        }
+    }
+    stream.synchronize()?;
+
+    let duration_cuda = start.elapsed();
+
+    let mut out_host = vec![0.0f32; mat_a_row * mat_b_col];
+    d_matrix_c.copy_to(&mut out_host)?;
+
+    // println!("matrix a  = {:?}",matrix_a);
+    // ntln!("matrix b = {:?}",matrix_b);
+
+    println!("result gpu  = {:?}", out_host);
+
+    let start_cpu = Instant::now();
+    let mut success_cpu = false;
+    let res_cpu = mul_matrix_cpu(
+        &matrix_a,
+        &matrix_b,
+        mat_a_row,
+        mat_a_col,
+        mat_b_row,
+        mat_b_col,
+        &mut success_cpu,
+    );
+    let duration_cpu = start_cpu.elapsed();
+
+    println!("result cpu  = {:?}", res_cpu);
+
+    if success_gpu {
+        println!("duration gpu: {:?}", duration_cuda);
+    } else {
+        println!("error calculating matrix mul for gpu ");
+    }
+
+    if success_cpu {
+        println!("duration cpu: {:?}", duration_cpu);
+    } else {
+        println!("error calculating matrix mul for cpu ");
+    }
+
+    for x in 0..res_cpu.len() {
+        // assert_eq!(res_cpu[x], out_host[x]);
+    }
+    // The kernel launch is asynchronous, so we wait for the kernel to finish executing
+
+    // show_device_props(&device);
+
+    Ok(())
+}
+
+fn mul_matrix_cpu(
+    a: &Vec<f32>,
+    b: &Vec<f32>,
+    mat_a_row: usize,
+    mat_a_col: usize,
+    mat_b_row: usize,
+    mat_b_col: usize,
+    success: &mut bool,
+) -> Vec<f32> {
+    let mut res: Vec<f32> = vec![0f32; mat_a_row * mat_b_col];
+
+    println!("res.len() = {}", res.len());
+
+    if mat_a_col != mat_b_row {
+        *success = false;
+        return res;
+    }
+    *success = true;
+
+    for row in 0..mat_a_row {
+        for col in 0..mat_b_col {
+            let mut tmp = 0f32;
+            for i in 0..mat_a_col {
+                let idx_a = i + row * mat_a_col;
+                let idx_b = i * mat_b_col + col;
+                //           println!("idx_a = {}, idx_b = {},    a.len = {},   b.len = {} ", idx_a, idx_b, a.len(), b.len() );
+                tmp = tmp + a[idx_a] * b[idx_b];
+            }
+            res[row * mat_b_col + col] = tmp;
+        }
+    }
+    res
+}
+
 fn add_matrix_cpu(a: &Vec<f32>, b: &Vec<f32>, nx: usize, ny: usize) -> Vec<f32> {
     let mut res: Vec<f32> = vec![0f32; nx * ny];
     for x in 0..nx {
@@ -260,6 +421,7 @@ fn add_matrix_cpu(a: &Vec<f32>, b: &Vec<f32>, nx: usize, ny: usize) -> Vec<f32> 
 
 fn main() -> Result<(), Box<dyn Error>> {
     // test_matrix()?;
-    add_matrix()?;
+    // add_matrix()?;
+    mul_matrix()?;
     Ok(())
 }
